c070f2c21b4e7c309748f8f7189961287dc4a0e4
Federico <frcpalma@gmail.com>
2021-04-18 14:39:25 +0200

    test sistemato

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -38,8 +38,8 @@ class RecipeSerializer(serializers.ModelSerializer):
             'price', 'link'
         )
         read_only_fields = ('id',)
-        
-        
+
+
 class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
